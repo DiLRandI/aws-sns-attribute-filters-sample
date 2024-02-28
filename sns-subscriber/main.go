544c6dd56ac8d927d2cf596076e9ba66b2627be0
Deleema Randimal <deleemarf@gmail.com>
2024-02-28 21:47:32 +0530

    update logs

--- sns-subscriber/main.go
+++ sns-subscriber/main.go
@@ -20,10 +20,20 @@ func HandleSNSMessage(ctx context.Context, snsEvent events.SNSEvent) error {
 	for _, record := range snsEvent.Records {
 		fmt.Printf("SNS Record: %s\n", record.SNS)
 		fmt.Printf("SNS MessageID: %s\n", record.SNS.MessageID)
+		fmt.Printf("SNS Signature: %s\n", record.SNS.Signature)
+		fmt.Printf("SNS Type: %s\n", record.SNS.Type)
+		fmt.Printf("SNS Timestamp: %s\n", record.SNS.Timestamp)
+		fmt.Printf("SNS TopicArn: %s\n", record.SNS.TopicArn)
+		fmt.Printf("SNS Subject: %s\n", record.SNS.Subject)
 		fmt.Printf("SNS Message: %s\n", record.SNS.Message)
-		fmt.Printf("SNS MessageAttributes: %s\n", record.SNS.MessageAttributes)
-		fmt.Printf("SNS MessageAttributes: %v\n", record.SNS.MessageAttributes)
-		fmt.Printf("SNS MessageAttributes: %v\n", record.SNS.MessageAttributes["jsonMessage"])
+
+		fmt.Println("...........................................................................")
+
+		for k, v := range record.SNS.MessageAttributes {
+			fmt.Printf("SNS MessageAttributes[%s]: %v\n", k, v)
+		}
+
+		fmt.Println("...........................................................................")
 	}
 
 	return nil
